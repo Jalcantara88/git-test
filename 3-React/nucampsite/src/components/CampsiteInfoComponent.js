abb20b2f04b5ff0a3862d18455ca4f78f114a07f
Julio Alcantara <jalcantara88@gmail.com>
2020-12-29 15:40:55 -0800

    functional components

--- 3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ 3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
-class CampsiteInfo extends Component {
-    renderComments(comments) {
+
+function RenderComments({comments}) {
         if(comments) {
             return(
                 <div className="col-md-5 m-1">
@@ -21,49 +21,47 @@ class CampsiteInfo extends Component {
                                         </CardBody>
                                     </Card>
                                 </div>
-                            )
+                            );
                         }
                     )}
                 </div>
-            )
+            );
         }
-        return(<div></div>)
-    }
+    return <div/>;
+}
 
-    renderCampsite(campsite) {
-        return (
-            <div className="col-md-5 m-1">
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name}/>
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>
-        )
-    }
-    render() {
-        console.log(this.props)
-        if(this.props.campsite) {
+function RenderCampsite({campsite}) {
+    return (
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src={campsite.image} alt={campsite.name}/>
+                <CardBody>
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
+
+function CampsiteInfo(props) {
+        
+        if(props.campsite) {
             return (
                 <div>
                     <div className="container">
                         <div className="row">
-                            {this.renderCampsite(this.props.campsite)}
-                            {this.renderComments(this.props.campsite.comments)}
+                            <RenderCampsite campsite={props.campsite}/>
+
+                            <RenderComments comments={props.comment}/>
                         </div>
                     </div>
                 </div>
             )
         }
-        return (
-            <div>
-               No Selected Campsite
-            </div>
-        )
+        return <div />;
     }
-}
+
 
 
 
