c35f52f8dc34688e3f70cde1c920d323cc40125c
Julio Alcantara <jalcantara88@gmail.com>
2021-01-06 14:18:30 -0800

    set up redux

--- 3-React/nucampsite/src/components/HeaderComponent.js
+++ 3-React/nucampsite/src/components/HeaderComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Nav, NavbarToggler, Collapse, NavItem, Navbar, NavbarBrand, Jumbotron, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
+
+//MAIN HEADER COMPONENT
 class Header extends Component {
 
     constructor(props) {
@@ -12,33 +14,36 @@ class Header extends Component {
             isNavOpen:false,
             isModalOpen: false
         };
-
+//BINDING METHODS TO CONSTUCTOR
         this.toggleNav = this.toggleNav.bind(this);
         this.toggleModal = this.toggleModal.bind(this);
         this.handleLogin = this.handleLogin.bind(this);
     }
-
+//INVERTS BOOL VALUE WHEN NAV IS TRIGGERED
     toggleNav() {
         this.setState({
             isNavOpen: !this.state.isNavOpen
         });
     }
-
+//INVERTS BOOL VALUE WHEN MODAL IS TRIGGERED
     toggleModal() {
         this.setState({
             isModalOpen: !this.state.isModalOpen
         });
     }
-
+//DISPLAYS INPUTS TO FORM ON SUBMIT CLICK
     handleLogin(event) {
         alert(`Username: ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`);
         this.toggleModal();
+        //STOPS ENTIRE PAGE FROM RE LOADING
         event.preventDefault();
     }
-
+//WHAT THIS COMPONENT RENDERS BACK
     render() {
         return(
+            //REACT FRAGMENT IS A WRAPPER PARENT DIV FOR OUTPUTTING MULTIPLE THINGS INSIDE OF
             <React.Fragment>
+{/LOGO HEADER/}
                 <Jumbotron fluid>
                     <div className="row">
                         <div className="col">
@@ -47,14 +52,22 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
+{/NAVBAR/}
                 <Navbar dark sticky="top" expand='md'>
                 <div className="container">
+{/BRAND IN NAVBAR/}
                     <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/logo.png" hight="30" width="30" alt="NuCamp Logo" /></NavbarBrand>
+{/TOGGLER ICON AND WHAT IT TRIGGERS ONCLICK/}
                     <NavbarToggler onClick={this.toggleNav} />
+{/EVERYTHING IN THIS COMPONENT WILL COLLAPSE AT SET MEDIA QUERRY/}
                     <Collapse isOpen={this.state.isNavOpen} navbar>
+{/NAVLINKS GROUP WRAPPER/}
                         <Nav navbar>
+{/NAVLINK HOLDER/}
                             <NavItem>
+{/NAVLINK/}
                                 <NavLink className="nav-link" to="/home">
+{/NAVLINK ICON/}
                                     <i className="fa fa-home fa-lg" />Home
                                 </NavLink>
                             </NavItem>
@@ -74,7 +87,7 @@ class Header extends Component {
                                 </NavLink>
                             </NavItem>
                         </Nav>
-
+{/LOGIN BUTTON OUTSIDE OF NAVLINK GROUP BUT STILL IN COLLAPSE/}
                         <span className="navbar-text ml-auto">
                             <Button outline onClick={this.toggleModal}>
                                 <i className="fa fa-sign-in fa-lg" /> Login
@@ -84,13 +97,15 @@ class Header extends Component {
                     </Collapse>
                 </div>
                 </Navbar>
-
+{/MODAL/}
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
                     <ModalBody>
+{/CONTACT FORM/}
                         <Form onSubmit={this.handleLogin}>
                             <FormGroup>
                                 <Label htmlFor="username">Username</Label>
+{/INNERREF HOLDS THE INPUT VALUE FOR YOU SO YOU CAN ACCESS THEM LATER WITH THE LOGIN HANDLER/}
                                 <Input type="text" id="username" name="username" innerRef={input => this.username = input}/>
                             </FormGroup>
 
@@ -105,6 +120,7 @@ class Header extends Component {
                                     Remember Me
                                 </Label>
                             </FormGroup>
+{/SUBMIT BUTTON/}
                             <Button type="submit" value="submit" color="primary">Login</Button>
                         </Form>
                     </ModalBody>
