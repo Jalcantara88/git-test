d77741e072e6faf295eb28132c8e6410a717ac3d
Julio Alcantara <jalcantara88@gmail.com>
2021-01-09 12:17:07 -0800

    redux add comment

--- 3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ 3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,8 +1,114 @@
-import React from 'react'
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
 import { Link } from 'react-router-dom'; 
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false
+        };
+//BINDING METHODS TO CONSTUCTOR
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleLogin(event) {
+        alert(`Username: ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`);
+        this.toggleModal();
+        //STOPS ENTIRE PAGE FROM RE LOADING
+        event.preventDefault();
+    }
+
+    handleSubmit(values) {
+        console.log("Current state is: " + JSON.stringify(values));
+        alert("Current state is: " + JSON.stringify(values));
+    
+    }
+
+    render() {
+        return(
+            <React.Fragment>
+                <Button outline onClick={this.toggleModal}>
+                <i className="fa fa-pencil fa-lg" />Submit Comment
+                </Button>
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+{/*CONTACT FORM*/}
+                    <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                            <div className="form-group">
+                                <Label htmlFor="rating" md={2}>Rating</Label>
+                                
+                                    <Control.select name="rating" model=".rating"
+                                    className="form-control">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Control.select>
+
+                                    
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="author" md={2}>Author</Label>
+                                
+                                    <Control.text model=".author" id="author" name="author"
+                                        placeholder="Author" className="form-control" 
+                                        validators={{
+                                            required,
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15),
+                                          }}
+                                    /> 
+
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                        required: "Required",
+                                        minLength: "Must be at least 2 characters",
+                                        maxLength: "Must be 15 characters or less",
+                                        }}
+                                    />                      
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="text" md={2}>Text</Label>
+        
+                                    <Control.textarea model=".text" id="text" name="text"
+                                        rows="6" className="form-control"
+                                    />
+                            </div>
+                                
+                            <Button type="submit" color="primary">
+                                Submit
+                            </Button>
+                                   
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </React.Fragment>
+            
 
 
+        );
+    }
+}
 
 function RenderComments({comments}) {
         if(comments) {
@@ -25,6 +131,9 @@ function RenderComments({comments}) {
                             );
                         }
                     )}
+                    <CommentForm />
+
+
                 </div>
             );
         }
